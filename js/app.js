0d685cd98ee37115ebaf30a12a8da70aefa7fcc6
TheDusty117 <gabrieledipassio@gmail.com>
2023-01-31 15:12:40 +0100

    FEAT adding subtasks

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
+console.log("working!")
+
+
 //1 chiedere all'utente numero km da percorrere
+
+  //1.1 definire quanto costa 1 km
+
 //2 chiedere all'utente eta'
+
+
+
 //3 applicare sconto 20% se sotto 18 anni
+
+  //3.1 definire che se <18 anni = -20%
+
+
+
 //4 applicare sconto 40% se sopra i 65 anni
+
+  //4.1 definire che se =>65 = -40%
+
+
+
 //5 prezzo finale scontato
