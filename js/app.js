aa25ce7b5c9594200b4abdf280484bcb32ca9470
TheDusty117 <gabrieledipassio@gmail.com>
2023-01-31 15:07:46 +0100

    FEAT task detection for js coding

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//1 chiedere all'utente numero km da percorrere
+//2 chiedere all'utente eta'
+//3 applicare sconto 20% se sotto 18 anni
+//4 applicare sconto 40% se sopra i 65 anni
+//5 prezzo finale scontato
