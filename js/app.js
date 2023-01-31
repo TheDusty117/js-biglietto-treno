af062294a859eea167681f1113faadf4710a6c0d
TheDusty117 <gabrieledipassio@gmail.com>
2023-01-31 16:38:20 +0100

    FEAT describing calculation process to discounts

--- js/app.js
+++ js/app.js
@@ -28,21 +28,92 @@ let askAge = prompt ("quanti anni hai?")
 console.log(askAge)
 
 
-//2.1 minore 18 anni
 
-let boyDiscount = (finalPrice/100*20) 
+
+
+//2.1 minore 18 anni----------------------------
+
+//definisco che boysDiscount equivale a (finalPrice/100*20)
+//ovvero l'importo da sottrarre a sua volta sempre a finalPrice
+
+let boysDiscount = (finalPrice/100*20) 
+
+// prezzo scontato under 18
+
+// definisco che discountedBoys equivale a finalPrice - boysDiscount
+// e quindi ad un 20%
+
+let discountedBoys = (finalPrice - boysDiscount)
+
+console.log(discountedBoys)
+
+
+
+
+//2.2 maggiore 65 anni-------------------------
+
+//definisco che grandPasDiscount equivale a (finalPrice/100*40)
+//ovvero l'importo da sottrarre a sua volta sempre a finalPrice
 
 let grandPasDiscount = (finalPrice/100*40)
 
-//2.2 maggiore 65 anni
+// prezzo scontato over 65
+
+// definisco che discountedBoys equivale a finalPrice - grandPasDiscount
+// e quindi ad un 40%
+
+let discountedGrandPas = (finalPrice - grandPasDiscount)
+
+console.log(discountedGrandPas)
+
+
+
+// se scelgo minore di 18 anni allora
+
+
+if (askAge < 18) {
+  finalPrice = discountedBoys
+}
+
+console.log(finalPrice)
+
+
+// se scelgo maggiore di 65 anni allora
+
+
+
+if (askAge > 65) {
+  finalPrice = discountedGrandPas
+}
+
+console.log(finalPrice)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 // finalPrice = finalPrice - 10;
 
-let discountedPrice = finalPrice - 10
+//  importante  ///////////
 
-console.log(discountedPrice)
+// let discountedPrice = finalPrice - 10
+
+// console.log(discountedPrice)
+
+
+
 
 //3 applicare sconto 20% se sotto 18 anni
 
